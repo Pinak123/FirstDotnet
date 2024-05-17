4557780670e9f84271b5b244e0567bdcbd8a32fe
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2024-05-17 21:55:20 +0530

    end of section 2

---
+++ DatingApp/Controller/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controller;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _dataContext;
+
+    public UsersController(DataContext dataContext)
+    {
+        _dataContext = dataContext;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var users =await _dataContext.User.ToListAsync();
+
+        return users;
+    }
+
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id )
+    {
+        return await _dataContext.User.FindAsync(id);
+    }
+}
---
+++ DatingApp/Entity/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.Entities;
+
+public class AppUser
+{
+   
+    public string  UserName { get; set; }
+    
+    [Key]
+    public int Id { get; set; }
+
+}
