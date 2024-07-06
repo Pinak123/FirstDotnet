d1cc8a520cd6f17df4e161c1a5c7de624cbbf814
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2024-07-06 17:38:09 +0530

    added new theme

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, TitleCasePipe } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -9,15 +9,16 @@ import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FormsModule , CommonModule , BsDropdownModule , RouterLink , RouterLinkActive],
+  imports: [FormsModule , CommonModule , BsDropdownModule , RouterLink , RouterLinkActive , TitleCasePipe],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   model:any ={};
+  user:any = {};
   constructor( public accountService:AccountService , public router:Router , private toastr:ToastrService)
   {
-    
+    this.user = accountService.currentUser();
   }
   ngOnInit():any{
 
@@ -26,7 +27,8 @@ export class NavbarComponent {
   login():any{
     this.accountService.login(this.model).subscribe({
       next: _ => {
-        this.router.navigateByUrl('/members')
+        this.router.navigateByUrl('/members');
+        
       },
       error: err=> this.toastr.error("invalid password")
     })
