5ed40a8f698e1a72ea99f13a7e4bc1f6d63a96ea
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2024-07-06 16:49:55 +0530

    done with auth gaurd

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,21 @@ import { MembersListsComponent } from './members/members-lists/members-lists.com
 import { MembersDetailsComponent } from './members/members-details/members-details.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './__gaurds/auth.guard';
 
 export const routes: Routes = [
     {path: '', component:HomeComponent},
-    {path: 'members', component:MembersListsComponent},
-    {path: 'members/:id', component:MembersDetailsComponent},
-    {path: 'lists', component:ListsComponent},
-    {path: 'messages', component:MessagesComponent},
+    {
+        path: '',
+        runGuardsAndResolvers:'always',
+        canActivate: [authGuard],
+        children: [
+            {path: 'members', component:MembersListsComponent , canActivate: [authGuard]},
+            {path: 'members/:id', component:MembersDetailsComponent},
+            {path: 'lists', component:ListsComponent},
+            {path: 'messages', component:MessagesComponent},
+        ]
+    },
+    
     {path: '**', component:HomeComponent  , pathMatch: 'full'}
 ];
